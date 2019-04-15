ce377ce16d3a0d2fee38c5fe0065a9b8bc505186
nemk96 <nemanjakostadinovic96@gmail.com>
2019-04-15 12:34:00 +0200

    test msg ok

--- src/test/testMessage.java
+++ src/test/testMessage.java
@@ -6,17 +6,16 @@ import tools.UserTools;
 public class testMessage {
 	
 	public  static void main(String[] args) {
-		JSONObject js = services.Message.addMessage("neecak",UserTools.getKey("neecak"),"salutNiko");
-		System.out.println(js);
+		//JSONObject js = services.Message.addMessage("neecak",UserTools.getKey("neecak"),"salutNiko");
+		//System.out.println(js);
 		
 		//boolean b = tools.MessageTools.addMessage(1, "nem", "sorry");
 		//System.out.println(b);
 		
-		//JSONObject js = services.Message.removeMessage("neecak",UserTools.getKey("neecak"),"5cb37512eeffb95cbd75d8ec");
+		//JSONObject js = services.Message.removeMessage("neecak",UserTools.getKey("neecak"),"5cb45d4eea0d8467f6a31cc5");
 		//System.out.println(js);
 		
 		//JSONObject js2 = services.Message.listMessages("neecak",UserTools.getKey("neecak"));
 		//System.out.println(js2);
 	}
-
 }
